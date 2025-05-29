f000338e7015ba5b66a61b4b3675fb08237c2a34
gulnara <tumarsal@gmail.com>
2025-05-29 14:12:37 +0300

    Задача 1,2

--- src/main/java/org/gulnara/dz/Main.java
+++ src/main/java/org/gulnara/dz/Main.java
@@ -4,5 +4,8 @@ public  class Main {
 
     public static void main(String[]  args){
         Task.task1();
-    }
+        Task.task2();
+
+
+}
 }
--- src/main/java/org/gulnara/dz/Task.java
+++ src/main/java/org/gulnara/dz/Task.java
@@ -2,9 +2,20 @@ package org.gulnara.dz;
 
 public class Task {
     public static void task1() {
-        System.out.println("----- Task 1 ------");
+        System.out.println("----- Задача 1 ------");
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
-}
+    public static void task2() {
+        int a = 7;
+        int b = -5;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("----- Задача 2 ------");
+            System.out.println("Сумма положительная");
+        }
+        else
+            System.out.println("Сумма отрицательная");
+        }
+    }
