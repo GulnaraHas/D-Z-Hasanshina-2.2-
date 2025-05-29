7017c5ce876fb5e46a512d34c4025739c83b69a5
gulnara <tumarsal@gmail.com>
2025-05-29 11:46:53 +0300

    Задача 1

--- src/main/java/org/gulnara/dz/Main.java
+++ src/main/java/org/gulnara/dz/Main.java
@@ -3,6 +3,6 @@ package org.gulnara.dz;
 public  class Main {
 
     public static void main(String[]  args){
-        System.out.println("dz");
+        Task.task1();
     }
 }
--- src/main/java/org/gulnara/dz/Task.java
+++ src/main/java/org/gulnara/dz/Task.java
@@ -1,7 +1,10 @@
 package org.gulnara.dz;
 
-public  class Task {
-    public static void task1(){
-
+public class Task {
+    public static void task1() {
+        System.out.println("----- Task 1 ------");
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 }
