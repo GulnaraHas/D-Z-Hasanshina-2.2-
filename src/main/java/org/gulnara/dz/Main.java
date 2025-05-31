be680f09b6411c43ef021057a522d9485b9d3a36
gulnara <tumarsal@gmail.com>
2025-05-31 13:45:53 +0300

    Задание к лекции 2.2 #1,2,3,4,5,6,7

--- src/main/java/org/gulnara/dz/Main.java
+++ src/main/java/org/gulnara/dz/Main.java
@@ -2,7 +2,7 @@ package org.gulnara.dz;
 
 public  class Main {
 
-    public static void main(String[]  args){
+    public static void main(String[] args) {
         Task.task1();
         Task.task2();
         Task.task3();
@@ -10,7 +10,6 @@ public  class Main {
         Task.task5();
         Task.task6(5);
         Task.task7(5);
-
-
-}
+        Task.task8(4);
+    }
 }
