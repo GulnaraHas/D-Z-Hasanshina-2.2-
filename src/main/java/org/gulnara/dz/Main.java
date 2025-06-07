66d46f1829d9548895f03334778645325a68967b
gulnara <tumarsal@gmail.com>
2025-06-07 18:43:35 +0300

    Задача 2 к лекции 2.3

--- src/main/java/org/gulnara/dz/Main.java
+++ src/main/java/org/gulnara/dz/Main.java
@@ -5,16 +5,25 @@ import java.time.LocalDate;
 class Main {
 
     public static void main(String[] args) {
-        // Создаем объект товара
-        Product product = new Product(
-                "Смартфон XYZ",
-                LocalDate.of(2023, 10, 15),
-                "TechCorp",
-                "Китай",
-                59999.99,
-                true
-        );
+        // Создаем массив из 5 товаров
+        Product[] productsArray = new Product[5];
 
-               product.printProductInfo();
+        // Заполняем массив товарами
+        productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025",
+                "Samsung Corp.", "Korea", 5599, true);
+        productsArray[1] = new Product("iPhone 16 Pro", "15.09.2024",
+                "Apple Inc.", "USA", 6499, false);
+        productsArray[2] = new Product("Xiaomi 14", "10.01.2024",
+                "Xiaomi", "China", 3999, true);
+        productsArray[3] = new Product("Nokia G60", "20.05.2023",
+                "HMD Global", "Finland", 2599, false);
+        productsArray[4] = new Product("Pixel 8 Pro", "12.10.2023",
+                "Google", "USA", 5299, true);
+
+        // Выводим информацию о всех товарах
+        for (Product product : productsArray) {
+            System.out.println("----------------------------------");
+            product.printProductInfo();
+        }
     }
 }
--- src/main/java/org/gulnara/dz/Product.java
+++ src/main/java/org/gulnara/dz/Product.java
@@ -1,5 +1,6 @@
 package org.gulnara.dz;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Product {
     private String name;                // Название товара
@@ -10,10 +11,12 @@ public class Product {
     private boolean isReserved;         // Состояние бронирования покупателем
 
     // Конструктор класса
-    public Product(String name, LocalDate productionDate, String manufacturer,
+    public Product(String name, String productionDate, String manufacturer,
                    String countryOfOrigin, double price, boolean isReserved) {
         this.name = name;
-        this.productionDate = productionDate;
+        // Парсим строку с датой в формате "dd.MM.yyyy" в LocalDate
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        this.productionDate = LocalDate.parse(productionDate, formatter);
         this.manufacturer = manufacturer;
         this.countryOfOrigin = countryOfOrigin;
         this.price = price;
@@ -22,7 +25,7 @@ public class Product {
 
     // Метод для вывода информации о товаре в консоль
     public void printProductInfo() {
-        System.out.println("Название: " + name);
+        System.out.println("Название: " + this.name);
         System.out.println("Дата производства: " + productionDate);
         System.out.println("Производитель: " + manufacturer);
         System.out.println("Страна происхождения: " + countryOfOrigin);
