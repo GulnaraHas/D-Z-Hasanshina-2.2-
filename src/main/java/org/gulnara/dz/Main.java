6860f2906787289464e36783fb47cd257b01a061
gulnara <tumarsal@gmail.com>
2025-06-08 21:51:48 +0300

    Задача 2 к лекции 2.4

--- src/main/java/org/gulnara/dz/Main.java
+++ src/main/java/org/gulnara/dz/Main.java
@@ -1,55 +1,20 @@
 package org.gulnara.dz;
 public class Main {
     public static void main(String[] args) {
-        // Создаем животных
-        Dog dog1 = new Dog("Бобик");
-        Dog dog2 = new Dog("Шарик");
-        Cat cat1 = new Cat("Мурзик");
-        Cat cat2 = new Cat("Барсик");
-        Cat cat3 = new Cat("Васька");
+        // Создаем фигуры
+        GeometricShape circle = new Circle(5, "Красный", "Черный");
+        GeometricShape rectangle = new Rectangle(4, 6, "Синий", "Белый");
+        GeometricShape triangle = new Triangle(3, 4, 5, "Зеленый", "Желтый");
 
-        // Тестируем бег и плавание
-        dog1.run(400);
-        dog1.run(600);
-        dog1.swim(5);
-        dog1.swim(15);
+        // Выводим информацию о фигурах
+        System.out.println("Круг:");
+        circle.displayInfo();
 
-        cat1.run(150);
-        cat1.run(250);
-        cat1.swim(1);
+        System.out.println("\nПрямоугольник:");
+        rectangle.displayInfo();
 
-        // Создаем миску и массив котов
-        Bowl bowl = new Bowl(30);
-        Cat[] cats = {cat1, cat2, cat3};
-
-        // Кормим котов
-        System.out.println("\nПытаемся накормить котов:");
-        for (Cat cat : cats) {
-            cat.eat(bowl, 15); // Каждый кот пытается съесть 15 единиц
-        }
-
-        // Проверяем сытость
-        System.out.println("\nСостояние котов:");
-        for (Cat cat : cats) {
-            System.out.println(cat.name + ": " + (cat.isFed() ? "сыт" : "голоден"));
-        }
-
-        // Добавляем еды и кормим снова
-        System.out.println("\nДобавляем еды в миску:");
-        bowl.addFood(20);
-
-        for (Cat cat : cats) {
-            if (!cat.isFed()) {
-                cat.eat(bowl, 15);
-            }
-        }
-
-        // Статистика
-        System.out.println("\nСтатистика животных:");
-        System.out.println("Всего животных: " + Animal.getAnimalCount());
-        System.out.println("Собак: " + Dog.getDogCount());
-        System.out.println("Котов: " + Cat.getCatCount());
+        System.out.println("\nТреугольник:");
+        triangle.displayInfo();
     }
 }
-
 
